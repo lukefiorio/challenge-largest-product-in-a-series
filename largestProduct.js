eef294c08dcc702f05a477128fb7024937c176da
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-19 09:42:39 -1000

    challenge completed

--- largestProduct.js
+++ largestProduct.js
@@ -1,6 +1,36 @@
 class LargestProduct {
   constructor(number) {
+    this.number = number;
+  }
 
+  getLargestProduct() {
+    let maxProduct = 0;
+    for (let i = 0; i < this.number.length - 5; i++) {
+      let maxProductCandidate = 1;
+      for (let j = 0; j < 5; j++) {
+        maxProductCandidate *= Number(this.number.charAt(i + j));
+      }
+      if (maxProductCandidate > maxProduct) {
+        maxProduct = maxProductCandidate;
+      }
+    }
+    return maxProduct;
+  }
+
+  getLargestProductPosition() {
+    let maxProduct = 0;
+    let position = 0;
+    for (let i = 0; i < this.number.length - 5; i++) {
+      let maxProductCandidate = 1;
+      for (let j = 0; j < 5; j++) {
+        maxProductCandidate *= Number(this.number.charAt(i + j));
+      }
+      if (maxProductCandidate > maxProduct) {
+        maxProduct = maxProductCandidate;
+        position = i;
+      }
+    }
+    return position;
   }
 }
 
